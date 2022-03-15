4a1659814c1692d2e9aaa5c2a57d7bdae90ac552
momo <1209816754@qq.com>
2022-03-15 05:17:26 -0700

    autostart patch and dwm statusbar

--- dwm.c
+++ dwm.c
@@ -207,6 +207,7 @@ static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void rotatestack(const Arg *arg);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1603,6 +1604,12 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	/* system("cd ~/.dwm; ./autostart_blocking.sh"); */
+	system("cd ~/scripts; /bin/bash ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2482,6 +2489,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
